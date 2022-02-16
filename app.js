85aa4f165f9fe495201def1c13a5add6f819f92b
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-02-16 22:25:54 +0600

    output adjusted

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInput = document.getElementById('income-input').value;
+    const foodInput = document.getElementById('food-input').value;
+    const rentInput = document.getElementById('rent-input').value;
+    const clothesInput = document.getElementById('clothes-input').value;
+
+})
