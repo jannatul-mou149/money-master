438c762720d511a3fa52f9f1b83ae4bf09a53136
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-02-16 23:59:57 +0600

    first part commiting

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const incomeInput = document.getElementById('income-input').value;
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothesInput = document.getElementById('clothes-input').value;
+    let incomeInput = document.getElementById('income-input').value;
+    let foodInput = document.getElementById('food-input').value;
+    let rentInput = document.getElementById('rent-input').value;
+    let clothesInput = document.getElementById('clothes-input').value;
 
+    var expenses = document.getElementById('expenses-total');
+    var balance = document.getElementById('balance');
+
+
+    var totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+    var balanceNew = incomeInput - totalExpenses;
+
+    expenses.innerText = totalExpenses;
+    balance.innerText = balanceNew;
 })
