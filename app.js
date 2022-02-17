4a97557856b3eec53b6b8565359be77a8a9aa8bc
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-02-17 09:25:25 +0600

    part part done with error handle

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    let incomeInput = document.getElementById('income-input').value;
+    incomeInput = document.getElementById('income-input').value;
     let foodInput = document.getElementById('food-input').value;
     let rentInput = document.getElementById('rent-input').value;
     let clothesInput = document.getElementById('clothes-input').value;
@@ -7,10 +7,27 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     var expenses = document.getElementById('expenses-total');
     var balance = document.getElementById('balance');
 
+    if (isNaN(incomeInput) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothesInput)) {
+        alert('You can not input a string');
+        location.href = "index.html";
+    }
 
-    var totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
-    var balanceNew = incomeInput - totalExpenses;
+    else if (incomeInput == "" || foodInput == "" || rentInput == "" || clothesInput == "") {
+        alert('You can not keep input feild empty');
+        location.href = "index.html";
+    }
+    else {
+        totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+        balanceNew = incomeInput - totalExpenses;
 
-    expenses.innerText = totalExpenses;
-    balance.innerText = balanceNew;
+        if (balanceNew < 0) {
+            alert('Opps!! Your Expenses are bigger than your income.');
+        }
+        else {
+            expenses.innerText = totalExpenses;
+            balance.innerText = balanceNew;
+        }
+    }
+
 })
+
